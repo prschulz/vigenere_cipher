73de84db02155ff3f80dc0b033b3f26f27e0967a
prschulz <prschulz88@gmail.com>
2015-02-18 15:42:07 -0800

    solution

--- src/vigenere.js
+++ src/vigenere.js
@@ -1,7 +1,30 @@
-var vigenere = {};
+vigenere = {};
 
 vigenere.encode = function(message, keyword) {
-}
+  var alphabet="abcdefghijklmnopqrstuvwxyz";
+  var rString = "";
+  for(var i in message){
+    i = parseInt(i);
+    var key = keyword[((i) % keyword.length)].charCodeAt(0) - 97;
+    console.log(alphabet[key]);
+    var ascii = (message[i].toLowerCase().charCodeAt(0) - 97 + key) % 26;
+    // console.log(alphabet[key],alphabet[ascii]);
+    rString += alphabet[ascii];
+  }
+  return rString;
+};
 
-// vigenere.decode = function(message, keyword) {
-// }
+
+
+vigenere.decode = function(message, keyword) {
+  var alphabet="abcdefghijklmnopqrstuvwxyz";
+  var rString = "";
+  for(var i in message){
+    i = parseInt(i);
+    var key = keyword[((i) % keyword.length)].charCodeAt(0) - 97;
+    var ascii = (26 + message[i].toLowerCase().charCodeAt(0) - 97 - key) % 26;
+    // console.log(alphabet[key],alphabet[ascii],ascii);
+    rString += alphabet[ascii];
+  }
+  return rString;
+};
